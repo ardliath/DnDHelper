857aacabc4b7f3fe3e642202129b17de1e470070
ardliath <ardliath@users.noreply.github.com>
2026-07-14 08:18:56 +0100

    Deploy stuff

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Repo is served from https://ardliath.github.io/DnDHelper/
+  base: '/DnDHelper/',
   plugins: [react()],
 })
